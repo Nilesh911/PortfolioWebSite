3b099ade75747a6dabb96cc42514c7cd7a8b0360
Nilesh119 <117853068+Nilesh119@users.noreply.github.com>
2023-07-15 16:29:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
   )
 }
 
